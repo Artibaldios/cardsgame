53c70c3182f7b6b6acfce0ee037b30c434c444c3
Artyom Borisevich <artibaldios@gmail.com>
2025-01-26 19:34:52 +0300

    Deploy

--- src/initialData/data.tsx
+++ src/initialData/data.tsx
@@ -1,20 +1,20 @@
 // Store image paths and names in an array
-import image1 from '../images/1.jpg';
-import image2 from '../images/2.jpg';
-import image3 from '../images/3.jpg';
-import image4 from '../images/4.jpg';
-import image5 from '../images/5.jpg';
-import image6 from '../images/6.jpg';
-import image7 from '../images/7.jpg';
-import image8 from '../images/8.jpg';
-import image9 from '../images/9.jpg';
-import image10 from '../images/10.jpg';
-import image11 from '../images/11.jpg';
-import image12 from '../images/12.jpg';
-import image13 from '../images/13.jpg';
-import image14 from '../images/14.jpg';
-import image15 from '../images/15.jpg';
-import image16 from '../images/16.jpg';
+import image1 from '/1.jpg';
+import image2 from '/2.jpg';
+import image3 from '/3.jpg';
+import image4 from '/4.jpg';
+import image5 from '/5.jpg';
+import image6 from '/6.jpg';
+import image7 from '/7.jpg';
+import image8 from '/8.jpg';
+import image9 from '/9.jpg';
+import image10 from '/10.jpg';
+import image11 from '/11.jpg';
+import image12 from '/12.jpg';
+import image13 from '/13.jpg';
+import image14 from '/14.jpg';
+import image15 from '/15.jpg';
+import image16 from '/16.jpg';
 
 const data = [
 	{ name: "Alex", image: image1 },
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cardsgame/",
+  base: '/cardsgame/',
+  build: {
+    outDir: 'dist/cardsgame', // Places all build files inside dist/cardsgame/
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // Keeps assets organized under assets/
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
   plugins: [react()],
 })
